bb89cf9b64352827d635ea637bb76307418c4180
Caroline <none>
2024-09-30 15:49:19 +0200

    Skapat klasser, fält och lista

---
+++ Activity.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+public class Activity
+{
+public string ActivityName {get; set;}
+public DateTime Date;
+public string Locations;
+private List<Scout>Participants{get; } = new List<Scout>();
+private string Notes;
+
+public Activity(string activityName)
+{
+    ActivityName = activityName;
+}
+
+
+public void AddParticipant(Scout scout)
+{
+
+    Participants.Add(scout);
+
+}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Scout scout1 = new Scout("Kalle", "Kalle@hotmail.com", "070112233", new DateOnly(2014,12,13));
+        Scout scout2 = new Scout("Bosse", "Bosse@hotmail.com", "070112244", new DateOnly(2015,10,30));
+        Scout scout3 = new Scout("Karin", "Bosse@hotmail.com", "070112244", new DateOnly(2016,09,22));
+        Scout scout4 = new Scout("Ida", "Bosse@hotmail.com", "070112244", new DateOnly(2014,02,19));
+        Scout scout5 = new Scout("Maryam", "Bosse@hotmail.com", "070112244", new DateOnly(2012,06,06));
+        Scout scout6 = new Scout("Filip", "Bosse@hotmail.com", "070112244", new DateOnly(2012,04,14));
+
+       
+       
+}
+}
